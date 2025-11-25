3f176900cb425d9be9bd353451cfe088ca90f672
Toko Migineishvili <toko.migine@gmail.com>
2025-11-25 16:59:05 +0400

    Create _middleware.js

---
+++ blog/src/app/api/_middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const res = NextResponse.next();
+
+  res.headers.set("Access-Control-Allow-Origin", "*"); // ან შენი frontend URL
+  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { status: 200, headers: res.headers });
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
